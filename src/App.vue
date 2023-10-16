ac6ebe0aa133e0f41e97c4a24848d15458ba83cb
IvanoPisaniello <ivanop92@gmail.com>
2023-10-16 15:56:27 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+
+  data() {
+    return {
+      projects: [],
+      pagination: {}
+
+    };
+  }
+
+
+}
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1 class="text-center">I MIEI PROGETTI</h1>
+    </div>
+
+  </main>
+</template>
