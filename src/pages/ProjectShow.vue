239bad0da408c1671e30a60a9d6ae86df27c61dc
IvanoPisaniello <ivanop92@gmail.com>
2023-10-17 18:48:07 +0200

    added content to show

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -1,11 +1,53 @@
 <script>
+import axios from "axios";
+
 
 export default {
+    data() {
+        return {
+            project: {},
+        };
+    },
+    methods: {
+        fetchData() {
 
+            axios
+                .get("http://localhost:8000/api/projects/" + this.$route.params.slug)
+                .then((resp) => {
+                    this.project = resp.data;
+                });
+        },
+        getImageUrl(project) {
+            return `http://127.0.0.1:8000/storage/${project.image}`;
+        },
 
-}
+    },
+    mounted() {
+        this.fetchData();
+    },
+};
 </script>
 
 <template>
-    <h1>contenuto della card</h1>
+    <div class="container">
+        <img :src="getImageUrl(project)" class="card-img-top" alt="..." />
+
+        <h1>{{ project.title }}</h1>
+
+        <div class="mb-3" v-if="project.technology">
+            Technologie:
+            <span class="badge" :style="`background-color: ${project.technology.color}`">{{ post.category.name }}</span>
+        </div>
+
+        <div class="d-flex gap-3">
+            <span v-for="technology in project.technologies" :key="project.id" class="badge"
+                :style="{ backgroundColor: technology.color, borderRadius: '10px', padding: '5px', marginRight: '5px' }">{{
+                    technology.name }}</span>
+        </div>
+        <p>{{ project.type.name }}</p>
+
+
+    </div>
 </template>
+
+<style scoped></style>
