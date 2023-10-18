51bb3acf96cb175c753d9b0a4ab56a9052623909
IvanoPisaniello <ivanop92@gmail.com>
2023-10-18 15:58:48 +0200

    fixed style progetti

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,27 +15,44 @@ export default {
 </script>
 
 <template>
-    <div class="my-card project-card card mb-3" style="border-radius: 10px;">
-        <img :src="getImageUrl(project)" class="card-img-top" alt="Project Image" style="border-radius: 10px;">
-        <div class="card-body">
-            <h5 class="card-title">{{ project.title }}</h5>
-            <p class="card-text">{{ project.description }}</p>
-            <div class="badges">
-                <div class="badge" v-for="technology in project.technologies"
-                    :style="{ backgroundColor: technology.color, borderRadius: '10px', padding: '5px', marginRight: '5px' }">
-                    {{ technology.name }}
+    <router-link class="custom-link" :to="{ name: 'projects.show', params: { slug: project.slug } }">
+        <div class="my-card project-card card mb-3" style="border-radius: 10px;">
+            <img :src="getImageUrl(project)" class="card-img-top" alt="Project Image" style="border-radius: 10px;">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ project.description }}</p>
+                <div class="badges">
+                    <div class="badge" v-for="technology in project.technologies"
+                        :style="{ backgroundColor: technology.color, borderRadius: '10px', padding: '5px', marginRight: '5px' }">
+                        {{ technology.name }}
+                    </div>
                 </div>
+                <p>{{ project.type.name }}</p>
+
             </div>
-            <p>{{ project.type.name }}</p>
-            <router-link class="fs-1 text-warning" :to="{ name: 'projects.show', params: { slug: project.slug } }"><i
-                    class="fa fa-eye"></i></router-link>
         </div>
-    </div>
+    </router-link>
 </template>
 
 
   
 
   
-<style scoped></style>
+<style scoped>
+.custom-link {
+    text-decoration: none;
+    color: inherit;
+}
+
+.my-card {
+    border-radius: 10px;
+    transition: box-shadow 0.3s, transform 0.3s;
+    cursor: pointer;
+}
+
+.my-card:hover {
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+    transform: scale(1.05);
+}
+</style>
   
