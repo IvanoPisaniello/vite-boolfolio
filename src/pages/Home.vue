0e05ef5f9ce4dc2b166d19d657c75a928ff418f5
IvanoPisaniello <ivanop92@gmail.com>
2023-10-18 16:22:30 +0200

    modified homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,50 +1,66 @@
 <script>
-
 export default {
-
-
 }
 </script>
 
 <template>
     <div class="homepage">
-        <div class="header">
-            <h1 class="name">Ivano Pisaniello</h1>
-            <p class="tagline">FullStuck Web Developer</p>
-        </div>
-        <div class="about">
-            <h2>Chi Sono</h2>
-            <p>
-                Benvenuto nel mio portfolio! Sono un appassionato sviluppatore web creativo e innovativo. Nel mio portfolio
-                troverai una vasta gamma di progetti che riflettono la mia dedizione e la mia passione.
-            </p>
-        </div>
-        <div class="contact">
-            <h2>Contattami</h2>
-            <p>
-                Se desideri collaborare o avere ulteriori informazioni sui miei progetti, non esitare a contattarmi.
-            </p>
-            <router-link class="nav-link" :to="{ name: 'contacts' }">Contattami</router-link>
+        <div class="content">
+            <div class="left-column">
+                <h1 class="name">Ivano Pisaniello</h1>
+                <p class="tagline">FullStack Web Developer</p>
+
+                <div class="about">
+                    <h2>Chi Sono</h2>
+                    <p>
+                        Benvenuto nel mio portfolio! Sono un appassionato sviluppatore web creativo e innovativo. Nel mio
+                        portfolio troverai una vasta gamma di progetti che riflettono la mia dedizione e la mia passione.
+                    </p>
+                </div>
+
+                <div class="contact">
+                    <h2>Contattami</h2>
+                    <p>
+                        Se desideri collaborare o avere ulteriori informazioni sui miei progetti, non esitare a contattarmi.
+                    </p>
+                    <router-link class="nav-link" :to="{ name: 'contacts' }">Contattami</router-link>
+                </div>
+            </div>
+            <div class="right-column">
+                <!-- Qui puoi inserire l'immagine -->
+            </div>
         </div>
     </div>
 </template>
   
 <style scoped lang="scss">
 .homepage {
-    text-align: center;
+    text-align: left;
+    /* Allineamento a sinistra */
     padding: 20px;
 }
 
+.content {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.left-column {
+    width: 60%;
+    /* Larghezza della colonna sinistra */
+}
+
 .name {
-    font-size: 48px;
+    font-size: 64px;
     font-weight: bold;
     margin: 10px 0;
-    color: #007BFF;
+    color: white;
 }
 
 .tagline {
     font-size: 24px;
-    color: #333;
+    color: white;
     margin: 10px 0;
 }
 
@@ -55,11 +71,13 @@ export default {
 .about h2 {
     font-size: 24px;
     font-weight: bold;
+    color: white;
 }
 
 .about p {
     font-size: 18px;
     line-height: 1.4;
+    color: white;
 }
 
 .contact {
@@ -69,22 +87,31 @@ export default {
 .contact h2 {
     font-size: 24px;
     font-weight: bold;
+    color: white;
 }
 
 .contact p {
     font-size: 18px;
     line-height: 1.4;
+    color: white;
+}
+
+.right-column {
+    width: 40%;
+    /* Larghezza della colonna destra */
+    background-color: #333;
+    /* Sfondo grigio per il div vuoto */
+    /* Puoi personalizzare ulteriormente il div vuoto a destra */
 }
 
 .contact a {
     font-size: 18px;
     text-decoration: none;
-    color: #007BFF;
+    color: white;
     transition: color 0.3s;
 }
 
 .contact a:hover {
     color: #0056b3;
-}
-</style>
+}</style>
   
